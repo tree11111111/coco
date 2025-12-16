27252ff4ab543effbb439dd12be186f431909fa0
coco-bebee <90363688+tree11111111@users.noreply.github.com>
2025-12-16 14:20:49 +0900

    Add files via GitUpload AI

--- api/classes/[id].ts
+++ api/classes/[id].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { storage } from '../../server/storage.ts';
+import { storage } from '../../server/storage';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
